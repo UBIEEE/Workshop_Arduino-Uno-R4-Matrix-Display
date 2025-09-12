c5bbf51c9831fea03ebac0b5118c925e392511f1
Eric-Butcher <107886303+Eric-Butcher@users.noreply.github.com>
2025-09-12 15:13:19 -0400

    Two frames alternating between a happy face and a heart.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,32 @@
 #include <Arduino.h>
 #include <Arduino_LED_Matrix.h>
 
-#define MAX_ROW 8
-#define MAX_COL 12
-
 ArduinoLEDMatrix matrix; // Create an instance of the ArduinoLEDMatrix class
-byte frame[8][12] = {
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+
+const uint32_t happy_frame[3] = {
+    0x19819,
+    0x80000001,
+    0x81f8000
 };
 
+// 0b110001100001001010010001000100
+// 0b1000010000010000001000100000000
+// 0b10100000000001000000000000000000
+const unsigned long heart_frame[3] = {
+  0x3184a444,
+  0x42081100,
+  0xa0040000};
+
+
+
 void setup() {
-  // Initialize the LED matrix
   matrix.begin();
-  matrix.renderBitmap(frame, MAX_ROW, MAX_COL);
 }
 
 void loop() {
-  const unsigned long delay_in_ms = 100;
-  for (int i = 0; i < MAX_ROW; i++) {
-    for (int j = 0; j < MAX_COL; j++) {
-      frame[i][j] = ~frame[i][j];
-      matrix.renderBitmap(frame, MAX_ROW, MAX_COL);
-      delay(delay_in_ms);
-    }
-  }
+  matrix.loadFrame(happy_frame);
+  delay(500);
+
+  matrix.loadFrame(heart_frame);
+  delay(500);
 }
